4371a76c72f37dfa13668b07a3efac3d163ff4b2
Vince Carey <stvjc@channing.harvard.edu>
2012-05-24 16:26:06 +0000

    added a tester of xvalSpec("NOTEST")
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_10/madman/Rpacks/MLInterfaces@66163 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testNOTEST.R
@@ -0,0 +1,33 @@
+
+library(MLInterfaces)
+
+    rf1 = try(MLearn(sp~CW+RW, data=crabs, randomForestI, xvalSpec("NOTEST"), ntree=600 ))
+    if (inherits(rf1, "try-error")) stop("xvalSpec('NOTEST') not working as intended with randomForestI")
+
+
+library(ALL)
+data(ALL)
+#
+# restrict to BCR/ABL or NEG
+#
+bio <- which( ALL$mol.biol %in% c("BCR/ABL", "NEG"))
+#
+# restrict to B-cell
+#
+isb <- grep("^B", as.character(ALL$BT))
+kp <- intersect(bio,isb)
+all2 <- ALL[,kp]
+mads = apply(exprs(all2),1,mad)
+kp = which(mads>1)  # get around 250 genes
+vall2 = all2[kp, ]
+vall2$mol.biol = factor(vall2$mol.biol) # drop unused levels
+
+rd1 = MLearn(mol.biol~., vall2, rdacvI, xvalSpec("NOTEST"))
+if (inherits(rd1, "try-error")) stop("xvalSpec('NOTEST') not working as intended with rdacvI")
+
+# following can be used to check whether we warn suitably for unanticipated NOTEST usage in xvalSpec
+#owarn = options()$warn
+#options(warn=2)
+#lk = try(MLInterfaces:::.check_NOTEST_COMPATIBILITY( svmI, xvalSpec("NOTEST") ))
+#if (!inherits(lk, "try-error")) stop(".check_NOTEST_COMPATIBILITY does not flag svmI")
+#options(warn=owarn)
